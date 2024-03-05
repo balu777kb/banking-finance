81411236042dcf0103347aee84c0efa3fb091636
balu777kb <157242039+balu777kb@users.noreply.github.com>
2024-03-05 21:21:05 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,17 @@
-#Initialize Terraform
-terraform {
+#terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
 
-# Configure the AWS provider
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
 }
+
 # Creating a VPC
 resource "aws_vpc" "proj-vpc" {
  cidr_block = "10.0.0.0/16"
